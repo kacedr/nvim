7fe90643cd8f2a4101d968d7a9c12b15869ff1c9
Kyle Gleason <Kyle.Gleason@panduit.com>
2025-07-30 10:25:21 -0500

    Changes

--- init.lua
+++ init.lua
@@ -5,11 +5,11 @@ require("core.keymaps")
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+        if vim.v.shell_error ~= 0 then
+                error("Error cloning lazy.nvim:\n" .. out)
+        end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 -- [[ Configure and install plugins ]]
@@ -24,27 +24,27 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 if not vim.g.vscode then
-	require("lazy").setup({
-        	require("plugins.neotree"),
-        	require("plugins.catppuccin"),
-        	require("plugins.lualine"),
-        	require("plugins.treesitter"),
-        	require("plugins.telescope"),
-        	require("plugins.lsp"),
-        	require("plugins.autocompletion"),
-        	require("plugins.null-ls"),
-        	require("plugins.alpha"),
-        	require("plugins.indent-blankline"),
-        	require("plugins.tmux-navigator"),
-        	require("plugins.trouble"),
-        	require("plugins.cinnamon"),
-        	require("plugins.noice"),
-        	require("plugins.winbar"),
-		require("plugins.misc"),
-	})
+        require("lazy").setup({
+                require("plugins.neotree"),
+                require("plugins.catppuccin"),
+                require("plugins.lualine"),
+                require("plugins.treesitter"),
+                require("plugins.telescope"),
+                require("plugins.lsp"),
+                require("plugins.autocompletion"),
+                require("plugins.null-ls"),
+                require("plugins.alpha"),
+                require("plugins.indent-blankline"),
+                require("plugins.tmux-navigator"),
+                require("plugins.trouble"),
+                require("plugins.cinnamon"),
+                require("plugins.noice"),
+                require("plugins.winbar"),
+                require("plugins.misc"),
+        })
 else
-	require("lazy").setup({
-		require("plugins.misc-vscode"),
-		require("plugins.cinnamon"),
-	})
+        require("lazy").setup({
+                require("plugins.misc-vscode"),
+                require("plugins.cinnamon"),
+        })
 end
